3684effaeda742dde76d8595d1c53cdd8580da02
antoZukko <38981409+antoZukko@users.noreply.github.com>
2018-08-28 15:54:41 +0200

    [dev] Added config.json file

--- node_server.js
+++ node_server.js
@@ -2,7 +2,10 @@ var express = require('express')
 var url = require('url')
 var app  = express()
 var path = require('path')
+var config = require('./config.json')
+var port = config.port
+
 
 app.use(express.static(__dirname + '/'))
 
-app.listen(8080)
+app.listen(port)
