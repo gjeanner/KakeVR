acf7be0688767d5878481e7a5391773d9d4db566
gaspardj <gaspard.jeannerot@capgemini.com>
2018-09-06 14:11:14 +0200

    Modif Paterne

--- script/module.js
+++ script/module.js
@@ -1,36 +1,29 @@
 
 function playVideo(video) {
   if(video){
-    console.log(video)
     if(video.paused){
       video.play()
       video.muted = false
-      //video.setAttribute('muted', true)
     }
   }
 }
 
 function pauseVideo(video) {
   if(video){
-    console.log(video)
     if(!video.paused){
       video.pause()
       video.muted = true
-      //video.setAttribute('muted', false)
     }
   }
 }
 
-
 function playSong(song) {
-  console.log("Le song lancé ",song)
   if(song) {
     song.components.sound.playSound()
   }
 }
 
 function pauseSong(song) {
-  console.log("Le song qui se pause ",song)
   if(song) {
     song.components.sound.pauseSound()
   }
@@ -48,15 +41,13 @@ init: function() {
   this.markersSong = document.querySelectorAll("a-marker.songMarker")
   this.songs = document.querySelectorAll("a-entity.song")
 
-  // this.songs[0].components.sound.playSound()
-
   this.markersVideoVisible = []
   this.markersVideoVisible.fill(false, 0, this.markersVideo.length)
 
   this.markersSongVisible = []
   this.markersSongVisible.fill(false, 0, this.markersSong.length)
   
-  this.tick = AFRAME.utils.throttleTick(this.tick, 250, this)
+  this.tick = AFRAME.utils.throttleTick(this.tick, 500, this)
 
 },
 
@@ -78,7 +69,6 @@ tick: function(t, dt) {
     }
     i++
   })
-
   
   if(!this.markersSong) return
   var i = 0
