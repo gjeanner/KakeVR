c7380e567a4b76eacda84ef4d5fc60bf2a5b36d6
gaspardj <gaspard.jeannerot@capgemini.com>
2018-09-07 15:59:44 +0200

    modification performance

--- script/module.js
+++ script/module.js
@@ -1,15 +1,19 @@
 
 function playVideo(video) {
   if(video){
+    video.play()
+    video.muted = false
     if(video.paused){
       video.play()
-      //video.muted = false
+      video.muted = false
     }
   }
 }
 
 function pauseVideo(video) {
   if(video){
+    video.pause()
+    video.muted = true
     if(!video.paused){
       video.pause()
       video.muted = true
@@ -29,66 +33,68 @@ function pauseSong(song) {
   }
 }
 
+AFRAME.registerComponent('songcomponent',{
+  init: function() {
+    this.markersSong = document.querySelectorAll("a-marker.songMarker")
+    this.songs = document.querySelectorAll("a-entity.song")
 
-AFRAME.registerComponent('artoolkit',{
-init: function() {
-  this.markersVideo = document.querySelectorAll("a-marker.videoMarker")
-  this.videos = document.querySelectorAll("video")
+    this.markersSongVisible = []
+    this.markersSongVisible.fill(false, 0, this.markersSong.length)
 
-  // entity = document.querySelector("a-entity#song")
-  // entity.components.sound.playSound()
-
-  this.markersSong = document.querySelectorAll("a-marker.songMarker")
-  this.songs = document.querySelectorAll("a-entity.song")
-
-  this.markersVideoVisible = []
-  this.markersVideoVisible.fill(false, 0, this.markersVideo.length)
-
-  this.markersSongVisible = []
-  this.markersSongVisible.fill(false, 0, this.markersSong.length)
-  
-  this.tick = AFRAME.utils.throttleTick(this.tick, 500, this)
-
-},
-
-tick: function(t, dt) {
-  
-  if (!this.markersVideo) return
-  var i = 0
-  this.markersVideo.forEach(marker => {
-    if (marker.object3D.visible) {
-      if (!this.markersVideoVisible[i]) {
-          this.markersVideoVisible[i] = true
-          playVideo(this.videos[i])
+    this.tick = AFRAME.utils.throttleTick(this.tick, 500, this)
+  },
+  tick: function(t, dt) {
+    if(!this.markersSong) return
+    var i = 0
+    this.markersSong.forEach(marker => {
+      if (marker.object3D.visible) {
+        if (!this.markersSongVisible[i]) {
+            this.markersSongVisible[i] = true
+            playSong(this.songs[i])
+        }
+      } else {
+        if (this.markersSongVisible[i]) {
+            this.markersSongVisible[i] = false
+            pauseSong(this.songs[i])
+        }
       }
-    } else {
-      if (this.markersVideoVisible[i]) {
-          this.markersVideoVisible[i] = false
-          pauseVideo(this.videos[i])
-      }
-    }
-    i++
-  })
-  
-  if(!this.markersSong) return
-  var i = 0
-  this.markersSong.forEach(marker => {
-    if (marker.object3D.visible) {
-      if (!this.markersSongVisible[i]) {
-          this.markersSongVisible[i] = true
-          playSong(this.songs[i])
-      }
-    } else {
-      if (this.markersSongVisible[i]) {
-          this.markersSongVisible[i] = false
-          pauseSong(this.songs[i])
-      }
-    }
-    i++
-  })
-  
+      i++
+    })
+    
+  }
+})
 
-}
+AFRAME.registerComponent('videocomponent',{
+  init: function() {
+    this.markersVideo = document.querySelectorAll("a-marker.videoMarker")
+    this.videos = document.querySelectorAll("video")
+
+    this.markersVideoVisible = []
+    this.markersVideoVisible.fill(false, 0, this.markersVideo.length)
+    
+    this.tick = AFRAME.utils.throttleTick(this.tick, 500, this)
+  },
+
+  tick: function(t, dt) {
+    
+    if (!this.markersVideo) return
+    var i = 0
+    this.markersVideo.forEach(marker => {
+      if (marker.object3D.visible) {
+        if (!this.markersVideoVisible[i]) {
+            this.markersVideoVisible[i] = true
+            playVideo(this.videos[i])
+        }
+      } else {
+        if (this.markersVideoVisible[i]) {
+            this.markersVideoVisible[i] = false
+            pauseVideo(this.videos[i])
+        }
+      }
+      i++
+    })
+
+  }
 })
 
 /*
